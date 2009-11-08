d59a2b33ab1e3b1f3509b92a522965d97623f1dc
Thibaut GRIDEL <tgridel@free.fr>
2009-11-08 23:10:52 +0100

    Slovenian translation

---
+++ locale/boats_sl.ts
@@ -0,0 +1,527 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="sl_SI">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>&amp;New File</source>
+        <translation>&amp;Nova datoteka</translation>
+    </message>
+    <message>
+        <source>Ctrl+N</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Open File...</source>
+        <translation>&amp;Odpri datoteko...</translation>
+    </message>
+    <message>
+        <source>Ctrl+O</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Save File</source>
+        <translation>&amp;Shrani datoteko</translation>
+    </message>
+    <message>
+        <source>Ctrl+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Save &amp;As...</source>
+        <translation>Shrani &amp;kot...</translation>
+    </message>
+    <message>
+        <source>Ctrl+Shift+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>New &amp;Tab</source>
+        <translation>Nov &amp;zavihek</translation>
+    </message>
+    <message>
+        <source>Ctrl+T</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Close Tab</source>
+        <translation>&amp;Zapri zavihek</translation>
+    </message>
+    <message>
+        <source>Ctrl+W</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Restore Last Session...</source>
+        <translation>&amp;Obnovi zadnjo sejo...</translation>
+    </message>
+    <message>
+        <source>&amp;Print...</source>
+        <translation>&amp;Natisni...</translation>
+    </message>
+    <message>
+        <source>Print &amp;Preview...</source>
+        <translation type="obsolete">Tiskanje &amp;Predogled...</translation>
+    </message>
+    <message>
+        <source>&amp;Export Pdf...</source>
+        <translation>&amp;Izvozi Pdf...</translation>
+    </message>
+    <message>
+        <source>Ctrl+E</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Export &amp;Image...</source>
+        <translation>Izvozi &amp;Sliko...</translation>
+    </message>
+    <message>
+        <source>Ctrl+I</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Export Ani&amp;mation...</source>
+        <translation>Izvozi &amp;animacijo...</translation>
+    </message>
+    <message>
+        <source>Ctrl+V</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Exit</source>
+        <translation type="obsolete">&amp;Izhod</translation>
+    </message>
+    <message>
+        <source>Ctrl+Q</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Exit the application</source>
+        <translation>Izhod iz programa</translation>
+    </message>
+    <message>
+        <source>Create &amp;Track</source>
+        <translation>Ustvari &amp;sled</translation>
+    </message>
+    <message>
+        <source>Ctrl+Ins</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Create &amp;Boat</source>
+        <translation>Ustvari &amp;plovilo</translation>
+    </message>
+    <message>
+        <source>Ins</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Create &amp;Mark</source>
+        <translation>Ustvari &amp;oznako</translation>
+    </message>
+    <message>
+        <source>Alt+Ins</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Port Overlap</source>
+        <translation>&amp;Levo prekrivanje</translation>
+    </message>
+    <message>
+        <source>Alt+&lt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Starboard Overlap</source>
+        <translation>&amp;Desno prekrivanje</translation>
+    </message>
+    <message>
+        <source>Alt+&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Text</source>
+        <translation>&amp;Besedilo</translation>
+    </message>
+    <message>
+        <source>Alt+T</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Toggle Mark &amp;Zone</source>
+        <translation>Izberi oznaki &amp;cono</translation>
+    </message>
+    <message>
+        <source>Z</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Delete Track</source>
+        <translation>Izbrisi sled</translation>
+    </message>
+    <message>
+        <source>Ctrl+Del</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Delete Selection</source>
+        <translation>&amp;Izbrisi izbrano</translation>
+    </message>
+    <message>
+        <source>Del</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Animate</source>
+        <translation>&amp;Animiraj</translation>
+    </message>
+    <message>
+        <source>Ctrl+A</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Play</source>
+        <translation>&amp;Predvajaj</translation>
+    </message>
+    <message>
+        <source>P</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Pause</source>
+        <translation>&amp;Premor</translation>
+    </message>
+    <message>
+        <source>M</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Stop</source>
+        <translation>&amp;Ustavi</translation>
+    </message>
+    <message>
+        <source>Space</source>
+        <translation>Presledek</translation>
+    </message>
+    <message>
+        <source>&amp;Loop</source>
+        <translation>&amp;Zanka</translation>
+    </message>
+    <message>
+        <source>L</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Undo</source>
+        <translation>&amp;Razveljavi</translation>
+    </message>
+    <message>
+        <source>Ctrl+Z</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Redo</source>
+        <translation>&amp;Ponovi</translation>
+    </message>
+    <message>
+        <source>Ctrl+Y</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Shift+Ctrl+Z</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Zoom &amp;In</source>
+        <translation>Zoom &amp;plus</translation>
+    </message>
+    <message>
+        <source>Ctrl++</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Zoom &amp;Out</source>
+        <translation>Zoom &amp;minus</translation>
+    </message>
+    <message>
+        <source>Ctrl+-</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Zoom &amp;Fit</source>
+        <translation>Zoom &amp;do prileganja</translation>
+    </message>
+    <message>
+        <source>Ctrl+F</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Main Toolbar</source>
+        <translation>Glavna orodna vrstica</translation>
+    </message>
+    <message>
+        <source>Animation Toolbar</source>
+        <translation>Animacijska orodna vrstica</translation>
+    </message>
+    <message>
+        <source>Scenario Dock</source>
+        <translation>Scenarij</translation>
+    </message>
+    <message>
+        <source>&amp;About</source>
+        <translation>&amp;O</translation>
+    </message>
+    <message>
+        <source>Create Track</source>
+        <translation>Ustvari sled</translation>
+    </message>
+    <message>
+        <source>Create Boat</source>
+        <translation>Ustvari plovilo</translation>
+    </message>
+    <message>
+        <source>Create Mark</source>
+        <translation>Ustvari oznako</translation>
+    </message>
+    <message>
+        <source>Animate</source>
+        <translation>Animiraj</translation>
+    </message>
+    <message>
+        <source>%1 - %2 [*]</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Boat Scenario</source>
+        <translation>Scenarij plovil</translation>
+    </message>
+    <message>
+        <source>&amp;Recent</source>
+        <translation>&amp;Zadnje</translation>
+    </message>
+    <message>
+        <source>&amp;File</source>
+        <translation>&amp;Datoteka</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation>&amp;Uredi</translation>
+    </message>
+    <message>
+        <source>&amp;History</source>
+        <translation>&amp;Zgodovina</translation>
+    </message>
+    <message>
+        <source>&amp;Animation</source>
+        <translation>&amp;Animacija</translation>
+    </message>
+    <message>
+        <source>&amp;Zoom</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;View</source>
+        <translation>&amp;Pogled</translation>
+    </message>
+    <message>
+        <source>&amp;%1 %2</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>The document %1 has been modified.
+Do you want to save your changes?</source>
+        <translation>Datoteka %1 je bila spremenjena.
+Ali zelite shraniti spremembe?</translation>
+    </message>
+    <message>
+        <source>Ctrl+P</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Ctrl+R</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Flag</source>
+        <translation>&amp;Zastava</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>&amp;Pomoc</translation>
+    </message>
+    <message>
+        <source>Choose &amp;Language</source>
+        <translation>Izberi &amp;jezik</translation>
+    </message>
+    <message>
+        <source>Open Scenario File</source>
+        <translation>Odpri datoteko scenarija</translation>
+    </message>
+    <message>
+        <source>xmlscenario Files (*.xbs)</source>
+        <translation>xmlscenarijske datoteke (*.xbs)</translation>
+    </message>
+    <message>
+        <source>Cannot read file %1:
+%2.</source>
+        <translation>Ne morem prebrati datoteke %1:
+%2.</translation>
+    </message>
+    <message>
+        <source>Open Scenario file</source>
+        <translation>Odpri datoteko scenarija</translation>
+    </message>
+    <message>
+        <source>Parse error in file %1 at line %2, column %3:
+%4</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>File loaded</source>
+        <translation>Nalozena datoteka</translation>
+    </message>
+    <message>
+        <source>Save Scenario</source>
+        <translation>Shrani scenarij</translation>
+    </message>
+    <message>
+        <source>%1 already exists.
+Do you want to replace it?</source>
+        <translation>%1 ze obstaja.
+Ali jo zelite zamenjati?</translation>
+    </message>
+    <message>
+        <source>Cannot write file %1:
+%2.</source>
+        <translation>Ne morem zapisati v datoteko %1:
+%2.</translation>
+    </message>
+    <message>
+        <source>File saved</source>
+        <translation>Datoteka je shranjena</translation>
+    </message>
+    <message>
+        <source>Print Document</source>
+        <translation>Natisni dokument</translation>
+    </message>
+    <message>
+        <source>Export Image</source>
+        <translation>Izvozi sliko</translation>
+    </message>
+    <message>
+        <source>Export Animation</source>
+        <translation>Izvozi animacijo</translation>
+    </message>
+    <message>
+        <source>Exporting Animation...</source>
+        <translation>Izvazam animacijo...</translation>
+    </message>
+    <message>
+        <source>Abort</source>
+        <translation>Prekini</translation>
+    </message>
+    <message>
+        <source>Protest!</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>About Boat Scenario</source>
+        <translation>O scenariju plovila</translation>
+    </message>
+    <message>
+        <source>Print P&amp;review...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>E&amp;xit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>The file is not an xmlscenario version 1.0 file.</source>
+        <translation>Datoteka ni xmlscenarij verzije 1.0.</translation>
+    </message>
+</context>
+<context>
+    <name>SituationPrint</name>
+    <message>
+        <source>Rule</source>
+        <translation>Pravilo</translation>
+    </message>
+</context>
+<context>
+    <name>SituationWidget</name>
+    <message>
+        <source>Options</source>
+        <translation>Moznosti</translation>
+    </message>
+    <message>
+        <source>Series</source>
+        <translation>Serija</translation>
+    </message>
+    <message>
+        <source>Show Laylines</source>
+        <translation>Prikazi Layline</translation>
+    </message>
+    <message>
+        <source>Laylines</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Zone Length</source>
+        <translation>Dolzina cone</translation>
+    </message>
+    <message>
+        <source>Tracks</source>
+        <translation>Sledi</translation>
+    </message>
+    <message>
+        <source>Scenario</source>
+        <translation>Scenarij</translation>
+    </message>
+    <message>
+        <source>Title</source>
+        <translation>Naslov</translation>
+    </message>
+    <message>
+        <source>Rules</source>
+        <translation>Pravila</translation>
+    </message>
+    <message>
+        <source>Abstract</source>
+        <translation>Povzetek</translation>
+    </message>
+    <message>
+        <source>Description</source>
+        <translation>Opis</translation>
+    </message>
+</context>
+<context>
+    <name>TrackDelegate</name>
+    <message>
+        <source>false</source>
+        <translation>napacno</translation>
+    </message>
+    <message>
+        <source>true</source>
+        <translation>resnicno</translation>
+    </message>
+</context>
+<context>
+    <name>TrackTableModel</name>
+    <message>
+        <source>Color</source>
+        <translation>Barva</translation>
+    </message>
+    <message>
+        <source>Path</source>
+        <translation>Pot</translation>
+    </message>
+    <message>
+        <source>Series</source>
+        <translation>Serija</translation>
+    </message>
+</context>
+</TS>
