3ccdb10455a3c56de848ede39b9478e4969e2e52
Thibaut Gridel <tgridel@free.fr>
2011-12-14 23:39:12 +0100

    copyright updates

--- trace.h
+++ trace.h
@@ -6,7 +6,7 @@
 //
 // Author: Thibaut GRIDEL <tgridel@free.fr>
 //
-// Copyright (c) 2008 Thibaut GRIDEL
+// Copyright (c) 2008-2009 Thibaut GRIDEL
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
